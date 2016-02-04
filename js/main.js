1ad4a584581404c9d7b59d08d6346c1099484b33
dashabespalova <dasha_box1@mail.ru>
2016-02-04 18:31:53 +0200

    first

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-;(function(){
+;(function($){
+	$(function(){
+		var slider = $('.ba-slider');
+		slider.slick({
+			dots:true,
+			gorizontal:true,
+			infinity: false
+		});
+		slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+			// console.log(nextSlide);
+			var currentSlideEl = $(slick.$slides[currentSlide]),
+			nextSlideEl = $(slick.$slides[nextSlide]),
+			currentClass = currentSlideEl.data('team'),
+			nextClass = nextSlideEl.data('team');
+			$('body').removeClass(currentClass).addClass(nextClass);
+		});
+	});
 
-})();
+})(jQuery);
